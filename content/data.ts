6418d4e10a0068182a647b04bb0b5d8e1d0ec2e4
Joseph___ <118039462+josephethiopia@users.noreply.github.com>
2024-11-30 10:59:53 +0300

    Add content/data.ts

---
+++ content/data.ts
@@ -0,0 +1,119 @@
+
+export const content = {
+  "home": {
+    "name": "Yoseph Ashenafi",
+    "description": "Fullstack Developer",
+    "location": "Addis Ababa, Ethiopia",
+    "availability": "Available",
+    "image": {
+      "src": "",
+      "alt": "Yoseph Ashenafi"
+    },
+    "socialLinks": {
+      "github": "",
+      "twitter": "",
+      "dribbble": ""
+    }
+  },
+  "aboutMe": {
+    "image": {
+      "src": "",
+      "alt": "Yoseph Ashenafi"
+    },
+    "aboutMe": "i'm joe a fullstack developer with 4 years of experience doing all perfect edge technologies for my clients",
+    "socialLinks": {
+      "twitter": "",
+      "github": ""
+    },
+    "quickFacts": {
+      "column1": [
+        "4+ Years Experience",
+        "Fullstack Developer"
+      ],
+      "column2": [
+        "Addis Ababa, Ethiopia",
+        "yosephashenfi.07@gmail.com"
+      ]
+    },
+    "outro": ""
+  },
+  "skills": {
+    "skills": [
+      {
+        "name": "React",
+        "icon": ""
+      },
+      {
+        "name": "Next.js",
+        "icon": ""
+      },
+      {
+        "name": "Tailwind CSS",
+        "icon": ""
+      },
+      {
+        "name": "Zustand",
+        "icon": ""
+      },
+      {
+        "name": "shadcn",
+        "icon": ""
+      },
+      {
+        "name": "Express.js",
+        "icon": ""
+      },
+      {
+        "name": "MongoDB",
+        "icon": ""
+      },
+      {
+        "name": "Node.js",
+        "icon": ""
+      },
+      {
+        "name": "MySQL",
+        "icon": ""
+      },
+      {
+        "name": "NextAuth",
+        "icon": ""
+      }
+    ]
+  },
+  "work": {
+    "projects": [
+      {
+        "title": "Inventory Management Software",
+        "description": "Worked on Inventory management software for a private client as a contract (fullstack)",
+        "image": "",
+        "technologies": [
+          "React",
+          "Next.js",
+          "Express.js",
+          "MongoDB",
+          "MySQL"
+        ]
+      },
+      {
+        "title": "Backend Training at Addis Ababa University",
+        "description": "Taught students at Addis Ababa University about backend development as a trainer for Google Developers Group.",
+        "image": "",
+        "technologies": [
+          "Node.js",
+          "Express.js",
+          "MongoDB",
+          "MySQL"
+        ]
+      }
+    ]
+  },
+  "contact": {
+    "email": "yosephashenfi.07@gmail.com",
+    "phone": "0909090909",
+    "header": "Contact Me",
+    "socialLinks": {
+      "telegram": "https:t.me/@joe"
+    }
+  }
+}
